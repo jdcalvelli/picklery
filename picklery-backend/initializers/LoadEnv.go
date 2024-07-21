21960d814b316f56fad540ae13f3a5e44b82c341
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2024-07-21 16:55:57 -0400

    moving .env location to root of dir, adding architecture check for make setup

--- picklery-backend/initializers/LoadEnv.go
+++ picklery-backend/initializers/LoadEnv.go
@@ -2,11 +2,13 @@ package initializers
 
 import (
 	"log"
+	"path/filepath"
+
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	envError := godotenv.Load()
+	envError := godotenv.Load(filepath.Join("..", ".env"))
 	  if envError != nil {
 		log.Fatal("Error loading .env file")
 	  }
